81346fe3f11faac4dc8b4a1eb05e52887e4298be
surafel7539 <suchaz7539@gmail.com>
2026-08-27 20:42:17 +0300

    s

--- Frontend/src/LIB/axios.js
+++ Frontend/src/LIB/axios.js
@@ -1,6 +1,6 @@
 import axios from'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "https://chat-app-ptjw.onrender.com/api",
     withCredentials: true,
 })
